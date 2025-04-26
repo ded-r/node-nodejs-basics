5c7b48c7d88da4254b1c39e1dac5d4927a7b244b
Didar Auyesbay <didarmaratov04@gmail.com>
2025-04-26 19:31:17 +0500

    hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const stream = createReadStream(path.join(import.meta.dirname, "files/fileToCalculateHashFor.txt"));
+    if (!stream) {
+        throw new Error("FS operation failed");
+    }
+    const hash = createHash("sha256");
+    stream.pipe(hash);
+
+    hash.on("readable", () => {
+        const data = hash.read();
+        if (!data) {
+            return;
+        }
+        console.log(data.toString("hex"));
+    });
 };
 
 await calculateHash();
